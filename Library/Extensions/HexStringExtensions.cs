8789b3ca1f5edad457e62b736e02136896811c77
Ben Thompson <ben@thompsondigital.com.au>
2023-07-24 19:27:38 +1000

    Removed obsolete replace

--- Library/Extensions/HexStringExtensions.cs
+++ Library/Extensions/HexStringExtensions.cs
@@ -6,21 +6,21 @@ public static class HexStringExtensions
 {
     public static String ToHexString(this UInt16 target, Int32 minBytes = 0)
     {
-        var output = target.ToString("X").Replace("-", "");
+        var output = target.ToString("X");
         output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
         return output;
     }
 
     public static String ToHexString(this UInt32 target, Int32 minBytes = 0)
     {
-        var output = target.ToString("X").Replace("-", "");
+        var output = target.ToString("X");
         output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
         return output;
     }
 
     public static String ToHexString(this UInt64 target, Int32 minBytes = 0)
     {
-        var output = target.ToString("X").Replace("-", "");
+        var output = target.ToString("X");
         output = new String('0', Math.Max(0, minBytes * 2 - output.Length)) + output;
         return output;
     }
